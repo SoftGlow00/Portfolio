4ee759f1ae74a29cc1f1971e539490c9df8e5759
SoftGlow <harunosakura000101@gmail.com>
2025-02-07 04:28:53 +0900

    apply vercel

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -43,6 +44,7 @@ function App() {
         </Routes>
         <Footer />
       </div>
+      <SpeedInsights/>
     </Router>
   );
 }
